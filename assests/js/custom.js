60448b8858f08b152e2955e6cc84cf5bd74d765c
javascript603 <react.dev@yahoo.com>
2019-01-08 22:22:42 -0500

    updated the custom js file

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -5,30 +5,16 @@ var editor = new MediumEditor('.editable', {
       forcePlainText: false
   }
 });
+
 $(function () {
   $('.editable').mediumInsert({
       editor: editor,
-      // addons: {
-      //     images: {
-      //         fileUploadOptions: {
-      //             url: 'upload.php'
-      //         }
-      //     }
-      // }
+      addons: {
+          images: {
+              fileUploadOptions: {
+                  url: 'upload.php'
+              }
+          }
+      }
   });
 });
-
-
-// $('#edit').on('click', function() {
-//   // this is the innermost *node*
-//   var an = window.getSelection().anchorNode;
-
-//   var pe = an.parentElement;
-//   var peC = pe.innerHTML;
-
-//   const parseData = getFind(peC);
-//   if (parseData) {
-//     const elements = createContent(parseData)
-//     updateContent(pe, elements);
-//   }
-// });
